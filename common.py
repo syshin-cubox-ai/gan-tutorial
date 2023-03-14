c38bc30c20a010ebfa0a3baffddbeae4a41e8b0c
신석용 <syshin@cubox.ai>
2023-03-14 17:26:45 +0900

    클래스로 구현

---
+++ common.py
@@ -0,0 +1,141 @@
+import os
+import random
+from typing import Tuple
+
+import cv2
+import numpy as np
+import torch.backends.cudnn
+import torch.nn as nn
+import torch.utils.data
+import torchvision
+import torchvision.transforms.functional as F
+import tqdm
+import wandb
+from PIL import Image
+
+
+class BaseGAN:
+    def __init__(
+            self,
+            config: dict,
+            generator: nn.Module,
+            discriminator: nn.Module,
+    ):
+        self.config = config
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.generator = generator.to(self.device)
+        self.discriminator = discriminator.to(self.device)
+        self.trainloader = None
+        self.criterion = None
+        self.g_optimizer = None
+        self.d_optimizer = None
+
+    def build_dataset(self):
+        transform = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize([0.5], [0.5]),
+        ])
+        trainset = torchvision.datasets.MNIST('data', True, transform, download=True)
+        self.trainloader = torch.utils.data.DataLoader(
+            trainset,
+            self.config['batch_size'],
+            shuffle=True,
+            num_workers=self.config['num_workers'],
+            prefetch_factor=self.config['prefetch_factor'],
+            persistent_workers=self.config['persistent_workers']
+        )
+
+    def build_criterion(self):
+        self.criterion = nn.BCELoss()
+
+    def build_optimizer(self):
+        self.g_optimizer = torch.optim.RAdam(self.generator.parameters(), self.config['lr'])
+        self.d_optimizer = torch.optim.RAdam(self.discriminator.parameters(), self.config['lr'])
+
+    def train_step(
+            self,
+            imgs: torch.Tensor,
+            labels: torch.Tensor,
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        raise NotImplementedError('Please implement training batch code.')
+
+    def train(self):
+        self.build_dataset()
+        self.build_criterion()
+        self.build_optimizer()
+
+        if self.config['reproducibility']:
+            torch.manual_seed(0)
+            torch.cuda.manual_seed(0)
+            torch.cuda.manual_seed_all(0)
+            torch.backends.cudnn.benchmark = False
+            torch.backends.cudnn.deterministic = True
+            np.random.seed(0)
+            random.seed(0)
+            torch.use_deterministic_algorithms(True)
+            os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
+
+        wandb.init(project=f'{self.config["model_name"]}', entity='syshin-cubox-ai', config=self.config)
+
+        d_loss = None
+        g_loss = None
+        real_score = None
+        fake_score = None
+        frames = []
+        for _ in tqdm.tqdm(range(self.config['epoch']), f'{self.config["model_name"]} Train', position=0):
+            for imgs, labels in tqdm.tqdm(self.trainloader, 'Batch', leave=False, position=1):
+                d_loss, g_loss, real_score, fake_score = self.train_step(imgs, labels)
+
+            # Get sample images
+            sample_img = self.infer(4)
+            frames.append(Image.fromarray(sample_img))
+
+            # Log to wandb
+            wandb.log({
+                'd_loss': d_loss.item(),
+                'g_loss': g_loss.item(),
+                'D(x)': real_score.mean().item(),
+                'D(G(z))': fake_score.mean().item(),
+                'sample_image': wandb.Image(sample_img),
+            })
+
+        # Save model
+        os.makedirs('weights', exist_ok=True)
+        torch.save({
+            'generator_state_dict': self.generator.state_dict(),
+            'discriminator_state_dict': self.discriminator.state_dict(),
+        }, os.path.join('weights', f'{self.config["model_name"].lower()}.pth'))
+
+        # Save sample images per epoch to gif
+        os.makedirs('results', exist_ok=True)
+        frames[0].save(os.path.join('results', f'train_{self.config["model_name"].lower()}.gif'), 'GIF',
+                       save_all=True, append_images=frames, duration=110, loop=0)
+
+        wandb.finish()
+
+    def infer(self, num_rows: int) -> np.ndarray:
+        raise NotImplementedError('Please implement inference code.')
+
+    @staticmethod
+    def post_process_sample_img(sample_img: torch.Tensor) -> np.ndarray:
+        sample_img = sample_img.reshape((-1, 1, 28, 28))
+        sample_img = F.normalize(sample_img, [-1 / 127 - 1], [1 / 127]).round().to(torch.uint8)
+        sample_img = torchvision.utils.make_grid(sample_img, 10, pad_value=110)
+        sample_img = np.ascontiguousarray(sample_img.permute((1, 2, 0)).cpu().numpy())
+        return sample_img
+
+    def demo(self, num_rows=10):
+        checkpoint = torch.load(os.path.join('weights', f'{self.config["model_name"].lower()}.pth'))
+        self.generator.load_state_dict(checkpoint['generator_state_dict'])
+
+        sample_image = self.infer(num_rows)
+
+        os.makedirs('results', exist_ok=True)
+        cv2.imwrite(os.path.join('results', f'{self.config["model_name"].lower()}.png'), sample_image)
+        print('Image creation complete.')
+
+    def select_training_or_demo(self) -> str:
+        if os.path.exists(os.path.join('weights', f'{self.config["model_name"].lower()}.pth')):
+            return 'Demo'
+        else:
+            return 'Train'
--- gan.py
+++ gan.py
@@ -1,114 +1,70 @@
-import os
-import random
+from typing import Tuple
 
-import cv2
 import numpy as np
 import torch.backends.cudnn
 import torch.nn as nn
 import torch.utils.data
-import torchvision
-import torchvision.transforms.functional as F
-import tqdm
-import wandb
-from PIL import Image
+
+import common
 
 
-def train(
-        epoch: int,
-        trainloader: torch.utils.data.DataLoader,
-        generator: nn.Module,
-        discriminator: nn.Module,
-        criterion: nn.Module,
-        g_optimizer: torch.optim.Optimizer,
-        d_optimizer: torch.optim.Optimizer,
-        device: torch.device,
-):
-    d_loss = None
-    g_loss = None
-    real_score = None
-    fake_score = None
-    frames = []
-    for eph in tqdm.tqdm(range(epoch), 'Epoch', position=0):
-        for images, labels in tqdm.tqdm(trainloader, 'Batch', leave=False, position=1):
-            images = torch.flatten(images, start_dim=1).to(device)
-            real_labels = torch.ones((images.shape[0], 1), device=device)
-            fake_labels = torch.zeros((images.shape[0], 1), device=device)
+class GAN(common.BaseGAN):
+    def __init__(
+            self,
+            config: dict,
+            generator: nn.Module,
+            discriminator: nn.Module,
+    ):
+        super().__init__(config, generator, discriminator)
 
-            # 판별자가 real 이미지를 real로 인식하는 loss 계산
-            real_score = discriminator(images)
-            d_loss_real = criterion(real_score, real_labels)
+    def train_step(
+            self,
+            imgs: torch.Tensor,
+            labels: torch.Tensor,
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        imgs = imgs.to(self.device)
+        real_labels = torch.ones((imgs.shape[0], 1), device=self.device)
+        fake_labels = torch.zeros((imgs.shape[0], 1), device=self.device)
 
-            # 랜덤 텐서로 fake 이미지 생성
-            z = torch.randn((images.shape[0], 64), device=device)
-            fake_images = generator(z)
+        # 판별자가 real 이미지를 real로 인식하는 loss 계산
+        real_score = self.discriminator(imgs)
+        d_loss_real = self.criterion(real_score, real_labels)
 
-            # 판별자가 fake 이미지를 fake로 인식하는 loss 계산
-            fake_score = discriminator(fake_images)
-            d_loss_fake = criterion(fake_score, fake_labels)
+        # 랜덤 텐서로 fake 이미지 생성
+        z = torch.randn((imgs.shape[0], 64), device=self.device)
+        sample_img = self.generator(z)
 
-            # real과 fake 이미지로 낸 오차를 더해서 최종 판별자 loss로 계산
-            d_loss = d_loss_real + d_loss_fake
+        # 판별자가 fake 이미지를 fake로 인식하는 loss 계산
+        fake_score = self.discriminator(sample_img)
+        d_loss_fake = self.criterion(fake_score, fake_labels)
 
-            # 판별자 모델 가중치 업데이트
-            d_optimizer.zero_grad(set_to_none=True)
-            d_loss.backward()
-            d_optimizer.step()
+        # real과 fake 이미지로 낸 오차를 더해서 최종 판별자 loss로 계산
+        d_loss = d_loss_real + d_loss_fake
 
-            # 생성자가 판별자를 속였는지에 대한 loss 계산
-            deception_score = discriminator(generator(z))
-            g_loss = criterion(deception_score, real_labels)
+        # 판별자 모델 가중치 업데이트
+        self.d_optimizer.zero_grad(set_to_none=True)
+        d_loss.backward()
+        self.d_optimizer.step()
 
-            # 생성자 모델 가중치 업데이트
-            g_optimizer.zero_grad(set_to_none=True)
-            g_loss.backward()
-            g_optimizer.step()
+        # 생성자가 판별자를 속였는지에 대한 loss 계산
+        deception_score = self.discriminator(self.generator(z))
+        g_loss = self.criterion(deception_score, real_labels)
 
-        # Get training information
-        d_loss = d_loss.item()
-        g_loss = g_loss.item()
-        real_score = real_score.mean().item()
-        fake_score = fake_score.mean().item()
+        # 생성자 모델 가중치 업데이트
+        self.g_optimizer.zero_grad(set_to_none=True)
+        g_loss.backward()
+        self.g_optimizer.step()
 
-        # Get sample fake images
-        fake_images = infer(generator, device, 40)
-        frames.append(Image.fromarray(fake_images))
+        return d_loss, g_loss, real_score, fake_score
 
-        # Log to wandb
-        wandb.log({
-            'd_loss': d_loss,
-            'g_loss': g_loss,
-            'D(x)': real_score,
-            'D(G(z))': fake_score,
-            'fake_images': wandb.Image(fake_images),
-        })
+    def infer(self, num_rows: int) -> np.ndarray:
+        self.generator.eval()
 
-        # Save model
-        os.makedirs('weights', exist_ok=True)
-        torch.save({
-            'generator_state_dict': generator.state_dict(),
-            'discriminator_state_dict': discriminator.state_dict(),
-            'epoch': eph,
-            'real_score': real_score,
-            'fake_score': fake_score,
-        }, os.path.join('weights', 'gan.pth'))
-
-    # Save fake images per epoch to gif
-    os.makedirs('results', exist_ok=True)
-    frames[0].save(os.path.join('results', 'train_gan.gif'), 'GIF',
-                   save_all=True, append_images=frames, duration=110, loop=0)
-
-
-def infer(generator: nn.Module, device: torch.device, num_images=100) -> np.ndarray:
-    generator.eval()
-
-    z = torch.randn((num_images, 64), device=device)
-    with torch.no_grad():
-        fake_images = generator(z)
-    fake_images = fake_images.reshape((-1, 1, 28, 28))
-    fake_images = F.normalize(fake_images, [-1 / 127 - 1], [1 / 127]).round().to(torch.uint8)
-    fake_images = torchvision.utils.make_grid(fake_images, 10, pad_value=110)
-    fake_images = np.ascontiguousarray(fake_images.permute((1, 2, 0)).cpu().numpy())
-    return fake_images
+        z = torch.randn((num_rows * 10, 64), device=self.device)
+        with torch.no_grad():
+            sample_img = self.generator(z)
+        sample_img = self.post_process_sample_img(sample_img)
+        return sample_img
 
 
 if __name__ == '__main__':
@@ -123,7 +79,6 @@ if __name__ == '__main__':
         'prefetch_factor': 60000,
         'persistent_workers': True,
     }
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # Model
     generator = nn.Sequential(
@@ -133,59 +88,19 @@ if __name__ == '__main__':
         nn.ReLU(),
         nn.Linear(256, 784),
         nn.Tanh(),
-    ).to(device)
+    )
     discriminator = nn.Sequential(
+        nn.Flatten(),
         nn.Linear(784, 256),
         nn.LeakyReLU(0.2),
         nn.Linear(256, 256),
         nn.LeakyReLU(0.2),
         nn.Linear(256, 1),
         nn.Sigmoid(),
-    ).to(device)
+    )
 
-    if os.path.exists(os.path.join('weights', 'gan.pth')):
-        checkpoint = torch.load(os.path.join('weights', 'gan.pth'))
-        generator.load_state_dict(checkpoint['generator_state_dict'])
-
-        fake_images = infer(generator, device)
-
-        os.makedirs('results', exist_ok=True)
-        cv2.imwrite(os.path.join('results', 'gan.png'), fake_images)
-        print('Image creation complete.')
+    gan = GAN(config, generator, discriminator)
+    if gan.select_training_or_demo() == 'Train':
+        gan.train()
     else:
-        # Pytorch reproducibility
-        if config['reproducibility']:
-            torch.manual_seed(0)
-            torch.cuda.manual_seed(0)
-            torch.cuda.manual_seed_all(0)
-            torch.backends.cudnn.benchmark = False
-            torch.backends.cudnn.deterministic = True
-            np.random.seed(0)
-            random.seed(0)
-            torch.use_deterministic_algorithms(True)
-            os.environ['CUBLAS_WORKSPACE_CONFIG'] = ':4096:8'
-
-        # Data
-        transform = torchvision.transforms.Compose([
-            torchvision.transforms.ToTensor(),
-            torchvision.transforms.Normalize((0.5,), (0.5,)),
-        ])
-        trainset = torchvision.datasets.MNIST('data', True, transform, download=True)
-        trainloader = torch.utils.data.DataLoader(
-            trainset,
-            config['batch_size'],
-            shuffle=True,
-            num_workers=config['num_workers'],
-            prefetch_factor=config['prefetch_factor'],
-            persistent_workers=config['persistent_workers']
-        )
-
-        # Loss function, optimizer
-        criterion = nn.BCELoss()
-        g_optimizer = torch.optim.RAdam(generator.parameters(), config['lr'])
-        d_optimizer = torch.optim.RAdam(discriminator.parameters(), config['lr'])
-
-        # Train
-        wandb.init(project='gan', entity='syshin-cubox-ai', config=config)
-        train(config['epoch'], trainloader, generator, discriminator, criterion, g_optimizer, d_optimizer, device)
-        wandb.finish()
+        gan.demo()
